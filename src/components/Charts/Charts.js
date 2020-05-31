94b9fd2307da294e4372602acd559e99abf4fca7
imvamsi <krrishvamsi7@gmail.com>
2020-05-31 17:39:40 -0400

    individual barchart added

--- src/components/Charts/Charts.js
+++ src/components/Charts/Charts.js
@@ -3,14 +3,36 @@ import { Line, Bar } from 'react-chartjs-2';
 import { fetchDailyInfectionRate } from '../../api';
 import styles from './Charts.module.css';
 
-const Charts = () => {
+const Charts = (props) => {
+  console.log(props);
   const [dailyRate, setDailyRate] = useState([]);
 
   useEffect(() => {
     fetchDailyInfectionRate()
       .then((res) => setDailyRate(res))
       .catch((err) => console.log(err));
-  }, [dailyRate]);
+  }, [setDailyRate]);
+
+  const barChart = (
+    props.data.confirmed ? (
+      <Bar
+        data={{
+          labels: ['Infected', 'Recovered', 'Deaths'],
+          datasets: [
+            {
+              label: 'People',
+              backgroundColor: ['rgba(0, 0, 255, 0.5)', 'rgba(0, 255, 0, 0.5)', 'rgba(255, 0, 0, 0.5)'],
+              data: [props.data.confirmed.value, props.data.recovered.value, props.data.deaths.value],
+            },
+          ],
+        }}
+        options={{
+          legend: { display: false },
+          title: { display: true, text: `Current state in ${props.country}` },
+        }}
+      />
+    ) : null
+  );
 
   const lineChart = dailyRate.length !== 0 ? (
     <Line
@@ -35,7 +57,7 @@ const Charts = () => {
       }}
     />
   ) : null;
-  return <div className={styles.container}>{lineChart}</div>;
+  return <div className={styles.container}>{props.country ? barChart : lineChart}</div>;
 };
 
 export default Charts;
